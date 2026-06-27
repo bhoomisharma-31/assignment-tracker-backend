c9175efe246e53211b6c48eaac93e027697a2583
bhoomisharma-31 <bhoomisharma3101@gmail.com>
2026-06-27 21:32:12 +0530

    Initial backend project

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Backend Running Successfully"
+    })
+
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    data = request.json
+
+    assignment = data.get("assignment")
+
+    return jsonify({
+        "status": "success",
+        "assignment": assignment
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
